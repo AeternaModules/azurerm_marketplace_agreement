9ff47cb3e460c2afe0dd9bece06fb345cf4d9aed
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:51 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "marketplace_agreements" {
-  description = "All marketplace_agreement resources"
-  value       = azurerm_marketplace_agreement.marketplace_agreements
-}
 output "marketplace_agreements_license_text_link" {
-  description = "List of license_text_link values across all marketplace_agreements"
-  value       = [for k, v in azurerm_marketplace_agreement.marketplace_agreements : v.license_text_link]
+  description = "Map of license_text_link values across all marketplace_agreements, keyed the same as var.marketplace_agreements"
+  value       = { for k, v in azurerm_marketplace_agreement.marketplace_agreements : k => v.license_text_link }
 }
 output "marketplace_agreements_offer" {
-  description = "List of offer values across all marketplace_agreements"
-  value       = [for k, v in azurerm_marketplace_agreement.marketplace_agreements : v.offer]
+  description = "Map of offer values across all marketplace_agreements, keyed the same as var.marketplace_agreements"
+  value       = { for k, v in azurerm_marketplace_agreement.marketplace_agreements : k => v.offer }
 }
 output "marketplace_agreements_plan" {
-  description = "List of plan values across all marketplace_agreements"
-  value       = [for k, v in azurerm_marketplace_agreement.marketplace_agreements : v.plan]
+  description = "Map of plan values across all marketplace_agreements, keyed the same as var.marketplace_agreements"
+  value       = { for k, v in azurerm_marketplace_agreement.marketplace_agreements : k => v.plan }
 }
 output "marketplace_agreements_privacy_policy_link" {
-  description = "List of privacy_policy_link values across all marketplace_agreements"
-  value       = [for k, v in azurerm_marketplace_agreement.marketplace_agreements : v.privacy_policy_link]
+  description = "Map of privacy_policy_link values across all marketplace_agreements, keyed the same as var.marketplace_agreements"
+  value       = { for k, v in azurerm_marketplace_agreement.marketplace_agreements : k => v.privacy_policy_link }
 }
 output "marketplace_agreements_publisher" {
-  description = "List of publisher values across all marketplace_agreements"
-  value       = [for k, v in azurerm_marketplace_agreement.marketplace_agreements : v.publisher]
+  description = "Map of publisher values across all marketplace_agreements, keyed the same as var.marketplace_agreements"
+  value       = { for k, v in azurerm_marketplace_agreement.marketplace_agreements : k => v.publisher }
 }
 
